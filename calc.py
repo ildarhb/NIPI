4d6b8e9f72c60217b752dc687804bcdba67ab27f
ildar <ahmetgalin989@gmail.com>
2023-05-20 15:39:46 +0500

    add k_h

--- calc.py
+++ calc.py
@@ -15,21 +15,6 @@ def thick_rezult_g():
             temp += item.thickness
     return temp
 
-
-def abs_thick_rezult_b():
-    temp = 0
-    for item in data:
-        if item.curr_saturation == "В":
-            temp += item.abs_thickness
-    return temp
-
-def abs_thick_rezult_g():
-    temp = 0
-    for item in data:
-        if item.curr_saturation == "Г":
-            temp += item.abs_thickness
-    return temp
-
 def porosity_rezult_b():
     temp = 0
     for item in data:
@@ -58,15 +43,32 @@ def permeability_rezult_g():
             temp += item.permeability*item.thickness
     return temp/thick_rezult_g()
 
+def k_h_otn():
+    temp1 = 0
+    temp2 = 0
+    for item in data:
+        if item.paker_isolation == "нет":
+            temp1 += (item.abs_thickness * item.permeability)/(abs_thick_rezult_b()*permeability_rezult_b()+abs_thick_rezult_g()*permeability_rezult_g())
+        else:
+            temp1 += 0
+    for item in data:
+        if item.paker_isolation == "нет":
+            temp2 += (item.abs_thickness * item.permeability)/(abs_thick_rezult_b()*permeability_rezult_b()+abs_thick_rezult_g()*permeability_rezult_g())*(1/temp1)
+        else:
+            temp2 += 0
+    return temp2
+
+
+
 result = {
-    "res1" : thick_rezult_b(),
-    "res2" : thick_rezult_g(),
-    "h_w, м": abs_thick_rezult_b(),
-    "h_g, м": abs_thick_rezult_g(),
+    "k_h_const" : k_h_otn(),
+    "h_w, м": thick_rezult_b(),
+    "h_g, м": thick_rezult_g(),
     "m_w, м": porosity_rezult_b(),
     "m_g, м": porosity_rezult_g(),
     "k_w, м": permeability_rezult_b(),
     "k_g, м": permeability_rezult_g(),
+    
 }
 
 for key,value in result.items():
--- initdata.py
+++ initdata.py
@@ -7,7 +7,7 @@ class InitGis():
     def __init__(self, name, plast, proplast, depth, 
                 thickness,abs_depth,abs_thickness,
                 porosity,permeability,lithology,
-                collector,init_saturation,curr_saturation):
+                collector,init_saturation,curr_saturation, paker_isolation):
         self.name = name
         self.plast = plast
         self.proplast = proplast
@@ -21,24 +21,40 @@ class InitGis():
         self.collector = collector
         self.init_saturation = init_saturation
         self.curr_saturation = curr_saturation
+        self.paker_isolation = paker_isolation
 
 def get_gis():    
     data = []
     for col in range(0,sheet.max_column):
         data.append(InitGis(
+            #интервал
             name = sheet[1][col].value,
+            #пласт
             plast = sheet[2][col].value,
+            #пропласток
             proplast = sheet[3][col].value,
+            #глубина
             depth = sheet[4][col].value,
+            #толщина
             thickness = sheet[5][col].value,
+            #глубина абсотлют
             abs_depth = sheet[6][col].value,
+            #тольщина абсолют
             abs_thickness = sheet[7][col].value,
-            porosity = sheet[8][col].value,
-            permeability = sheet[9][col].value,
-            lithology = sheet[10][col].value,
+            #пористость
+            porosity = sheet[8][col].value if sheet[8][col].value != None else 0, 
+            #проницаемость
+            permeability = sheet[9][col].value if sheet[9][col].value != None else 0,
+            #литология
+            lithology = sheet[10][col].value if sheet[10][col].value != None else 0,
+            #коллектор
             collector = sheet[11][col].value,
+            #начальная насыщенность
             init_saturation = sheet[12][col].value,
-            curr_saturation = sheet[13][col].value))
+            #конченая насыщенность
+            curr_saturation = sheet[13][col].value,
+            #склективаня изоляция пакером
+            paker_isolation = sheet[14][col].value))
     return data
 
 
