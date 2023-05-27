56c30e1173adcfc20bd9c61fefc11fbb3f3b5648
Murloc <i-volzhin@mail.ru>
2023-05-27 12:32:18 +0500

    коммит перед мерджем

--- Application/main.py
+++ Application/main.py
@@ -11,20 +11,35 @@ class Window(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.load_data()
+        # self.load_data()
         self.ui.btnGetData.clicked.connect(self.btnGetDataClicked)
 
     def load_data(self):
-        self.ui.tableWidget.setRowCount(3)
-        self.ui.tableWidget.setColumnCount(2)
+        # Загружает данные в таблицу из входного Excel
 
-        self.ui.tableWidget.setHorizontalHeaderLabels(('Liq', 'Oil'))
+        # self.ui.tableWidget.setItem(0, 0, QTableWidgetItem('Test value'))
+        # self.ui.tableWidget.setItem(0, 1, QTableWidgetItem('5000 tonn'))
+        try:
+            file_data = initdata.get_gis("../doc/gis.xlsx")
+        except:
+            raise ImportError("Не удалось прочитать файл с данными")
 
-        self.ui.tableWidget.setItem(0, 0, QTableWidgetItem('Test value'))
-        self.ui.tableWidget.setItem(0, 1, QTableWidgetItem('5000 tonn'))
+        if len(file_data) == 0:  # если нет столбцов, то ничего не заполняем
+            return
+
+        self.ui.tableWidget.setColumnCount(len(file_data))  # устанавливаем количество столбцов
+        columns = file_data[0].__dict__  # получаем все переменные класса
+        self.ui.tableWidget.setRowCount(len(columns))  # устанавливаем количество строк
+        self.ui.tableWidget.setVerticalHeaderLabels(columns.keys())  # даем названия строкам
+
+        # Заполняем данными таблицу
+        for column_index, column in enumerate(file_data):
+            for row_index, row in enumerate(columns):
+                self.ui.tableWidget.setItem(row_index, column_index, QTableWidgetItem(str(column.__getattribute__(row))))
 
     def btnGetDataClicked(self):
-        print("Works!")
+        self.ui.tableWidget.clear()
+        self.load_data()
 
 
 def create_app():
@@ -36,4 +51,4 @@ def create_app():
 
 if __name__ == "__main__":
     create_app()
-    data = initdata.get_gis("../doc/gis.xlsx")
+    # data = initdata.get_gis("../doc/gis.xlsx")
