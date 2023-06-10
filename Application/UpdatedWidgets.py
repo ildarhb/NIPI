54b49e13ea4cad391f0e6a36bbed5a125fcae394
Murloc <i-volzhin@mail.ru>
2023-06-10 19:28:57 +0500

    Добавление заполнения данных до обводнения и после обводнения

---
+++ Application/UpdatedWidgets.py
@@ -0,0 +1,31 @@
+from PyQt5.QtWidgets import QTableWidget
+
+
+class UpgradedQTableWidget(QTableWidget):
+
+    def fill_names(self, rows, columns):
+        self.VerticalHeaderLabels = rows
+        self.HorizontalHeaderLabels = columns
+
+        self.setRowCount(len(rows))
+        self.setVerticalHeaderLabels(rows)
+
+        self.setColumnCount(len(columns))
+        self.setHorizontalHeaderLabels(columns)
+
+    def get_table_items(self):
+        row_count = self.rowCount()
+        column_count = self.columnCount()
+        table_data = list()
+
+        for column_index in range(column_count):
+            current_row = (self.item(row_index, column_index) for row_index in range(row_count))  # столбец таблицы
+            current_row_text = (item.text() if item is not None else "" for item in current_row)  # текстовый столбец
+            table_data.append(tuple(current_row_text))
+
+        return table_data
+
+    def get_dict_column(self, index=0):
+        items = self.get_table_items()[index]
+        names = self.VerticalHeaderLabels
+        return {key: value for key, value in zip(names, items)}
