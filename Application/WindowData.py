a469199bde15580880ba7921b538b50edfbfd159
Murloc <i-volzhin@mail.ru>
2023-06-12 13:31:19 +0500

    Реализация выбора трех полимеров, в классе WindowData.py теперь gelling - это словарь из трех выбранных полимеров

--- Application/WindowData.py
+++ Application/WindowData.py
@@ -1,6 +1,6 @@
 class WindowData:
     def __init__(self):
         self.gis = list()
-        self.gelling = None
+        self.gelling = dict()
         self.gis_after_watering = None
         self.gis_before_watering = None
